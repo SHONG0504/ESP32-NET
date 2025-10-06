cb939d41e90fde343887453719ca0b760de32907
Eunbin Choe <dmsqls010414@gmail.com>
2025-10-05 21:21:36 -0400

    Testing

--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,24 @@
-extern "C" {
+#include "esp_log.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+extern "C"
+{
     void app_main(void);
+}
+
+void print_task(void *args)
+{
+    uint32_t a = 0;
+    for (;;)
+    {
+        ESP_LOGI("PRINT", "Hello %d", a++);
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
 }
 
 void app_main(void)
 {
-
+    xTaskCreate(&print_task, "PRINT", 2048, NULL, 5, NULL);
 }
